a99324f0d7f3c3ac5770c33264f2986f7e1ba0b1
Timur Turatbekov <timatur242005@gmail.com>
2021-09-07 19:02:48 +0300

    semi colons

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 function createDeps() {
-  const serviceAccount = require(process.env.FIREBASE_CREDENTIAL_PATH)
+  const serviceAccount = require(process.env.FIREBASE_CREDENTIAL_PATH);
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL
-  })
+  });
   const firestore = firebaseAdmin.firestore();
 
-  return { firestore }
+  return { firestore };
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Привет 👋! Чтобы узнать список доступных команд напиши /help'))
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Привет 👋! Чтобы узнать список доступных команд напиши /help'));
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
